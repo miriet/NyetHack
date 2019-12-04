d50b05aacb539d8dd75a2332fc2b8e804b69a07a
miriet@naver.com <miriet@naver.com>
2019-12-04 21:48:46 +0900

    null처리 테스트

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -2,6 +2,9 @@ fun main(args: Array<String>){
 //    var signatureDrink = "맥주"
 //    signatureDrink = null
     var signatureDrink = readLine()
+        signatureDrink?.let {
+        signatureDrink = it.capitalize()
+    } ?: println("null입니다.")
 //    signatureDrink = null
     println(signatureDrink)
 }
